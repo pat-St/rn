2ec290764ba340eba81743286e18e75aa26a142c
patrick sterk <patrick.sterk@protonmail.com>
2018-12-02 00:16:23 +0100

    test

--- chat.py
+++ chat.py
@@ -104,7 +104,7 @@ def scanNetwork():
     sendNickname = 'S ' + username
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(120)
-    for i in range(14, 64):  # range(LowestIP, HighestIP):
+    for i in 15, 62:  # range(LowestIP, HighestIP):
         newHostIP = HOST + str(i)
         try:
             sock.connect((newHostIP, PORT))
