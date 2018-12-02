345bda4e585ee9f6c24cc287db13fc0b4ba0f786
patrick sterk <patrick.sterk@protonmail.com>
2018-12-02 14:35:37 +0100

    test

--- chat.py
+++ chat.py
@@ -138,7 +138,7 @@ def sendMessage(nickname, message):
     for key, value in activeUser.items():
         if value == nickname:
             try:
-                key.send(('C ' + "".join(message)).encode("utf-8"))
+                key.send(('C ' + " ".join(message)).encode("utf-8"))
             except (ConnectionRefusedError, ConnectionAbortedError, BrokenPipeError, IndexError, OSError,
                     socket.timeout) as err:
                 print("send Message Error: " + str(err))
